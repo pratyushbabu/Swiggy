47cd00cac49cc6167e3088f2b6413fc9dcd0876b
Arpan-creator <arpan.9932176726@gmail.com>
2025-06-18 01:40:31 +0530

    app changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,50 @@ import './index.css'
 import {Routes,Route} from "react-router-dom"
 
 import Cart from "./pages/Cart"
+import { useLocation } from 'react-router-dom';
+
 
 
 function App() {
   // const [count, setCount] = useState(0)
   const [foodName,setFoodName]=useState("")
 
+  const [restaurantId,setRestaurantId]=useState()
+
+  const location = useLocation();
+
+  const hideFooterPaths = ['/cart']; // Add more paths if needed
+  const shouldShowFooter = !hideFooterPaths.includes(location.pathname);
+
   return (
       <div>
-        <Head/>
+      <Head />
         <Routes>
-          <Route path="/restaurants" element={<>
-          <Categories foodName={foodName} setFoodName={setFoodName}/>
-          <Resturent name={foodName} setFoodName={setFoodName}/>
-          </>}/>
-          
-          <Route path="/cart" element={<Cart/>}/>
+        <Route
+          path="/"
+          element={
+            <>
+              <Categories
+                foodName={foodName}
+                setFoodName={setFoodName}
+              />
+              <Resturent
+                name={foodName}
+                setFoodName={setFoodName}
+                setRestaurantId={setRestaurantId}
+              />
+            </>
+          }
+        />
+        <Route
+          path="/cart"
+          element={<Cart restaurantId={restaurantId} />}
+        />
         </Routes>
         
-   
-        <Footer/>
-        {/* <Body/> */}
+      {shouldShowFooter && <Footer />}
       </div>
-
-  )
+  );
 }
 
 export default App
